8b8b19b59a6472c60d30f5dc98a5497d1ef16cb7
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-26 14:46:30 +0200

    risolvi stampe di valori

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,10 +2,18 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+const name = "Riccardo";
+console.log(name); 
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+const number = 8;
+const text = "Ciaooooooo";
+const bool = true;
+console.log(number, text, bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
